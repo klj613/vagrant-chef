45ff3d14d5f57b2da8783a3fef2ffee85f8c1755
Kristian Lewis Jones <klj613@kristianlewisjones.com>
2013-02-18 23:36:16 +0000

    Adds pub key for epel repo

--- common/cookbooks/epel/recipes/default.rb
+++ common/cookbooks/epel/recipes/default.rb
@@ -1,6 +1,12 @@
 rpm_url = "https://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm"
 rpm = "epel-release-6-8.noarch"
+key = "https://fedoraproject.org/static/0608B895.txt" 
 path = "#{Chef::Config[:file_cache_path]}/#{rpm}"
+
+execute "import" do
+  command "rpm --import #{key}" 
+  action :run
+end
 
 remote_file path do
   action :create_if_missing
